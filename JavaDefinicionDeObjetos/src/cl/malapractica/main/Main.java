482ea1c677a79412439f2a8e3877e7d9b42932c8
jazocar <jazocar@gasco.cl>
2022-11-22 19:34:58 -0300

    Commit for JavaDefinicionDeObjetos

---
+++ JavaDefinicionDeObjetos/src/cl/malapractica/main/Main.java
@@ -0,0 +1,30 @@
+package cl.malapractica.main;
+
+import cl.malapractica.entidades.Persona;
+import java.util.Date;
+
+/**
+ *
+ * @author malapractica
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Instanciamos la clase Persona
+        Persona persona = new Persona();
+        //Llenamos los mutadores de la clase
+        persona.setDni("12345678-2");
+        persona.setPrimerNombre("John");
+        persona.setSegundoNombre("");
+        persona.setPrimerApellido("Doe");
+        persona.setEdad(34);
+        persona.setFechaNacimiento(new Date());
+        //Llamamos al método toString().
+        System.out.println(persona.toString());
+    }
+    
+}
